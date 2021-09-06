b291e558e2e62b0c6c7e2643bd7ba2490695556c
svetlin <svetlin.stalinov@monetdbsolutions.com>
2021-09-06 08:57:40 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import os, sys
+from flask import Flask
+from flask import render_template
+from flask import request, jsonify, abort
+from mimetypes import guess_extension
+from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    if "audio_file" in request.files:
+        file = request.files['audio_file']
+        dst = os.path.join(os.path.curdir, 'uploads', "test_audio.webm")
+        file.save(dst)
+    return jsonify(ok=True)
+
---
+++ static/app.js
@@ -0,0 +1,57 @@
+(function () {
+    const startBtn = document.getElementById("startBtn");
+    const stopBtn = document.getElementById("stopBtn");
+    const form = document.querySelector('form');
+    const submitBtn = form.querySelector('button');
+
+    if(navigator.mediaDevices) {
+        navigator.mediaDevices.getUserMedia({audio: true})
+            .then(stream => {
+                const recorder = new MediaRecorder(stream);
+
+                const chunks = [];
+
+                stopBtn.disabled = true;
+                submitBtn.disabled  = true;
+
+                startBtn.addEventListener('click', (e) => {
+                    if (recorder.state !== 'recording') {
+                        recorder.start();
+                        stopBtn.disabled = false;
+                    }
+                    console.log(recorder.state);
+                });
+
+                stopBtn.addEventListener('click', (e) => {
+                    if (recorder.state !== "inactive") {
+                        chunks.push(recorder.requestData());
+                        recorder.stop();
+                        e.target.disabled = true;
+                        submitBtn.disabled = false;
+                    }
+                    console.log(recorder.state);
+                });
+
+                form.addEventListener('submit', (e) => {
+                    e.preventDefault();
+                    const fd = new FormData();
+                    const blob = new Blob(chunks, {'type' : recorder.mimeType })
+                    console.log("->>>>", recorder.mimeType);
+                    fd.append('audio_file', blob);
+                    fetch('http://localhost:5000/upload', {method: "POST", cache: "no-cache", body: fd})
+                        .then(resp => {
+                            if(resp.status === 200) {
+                                window.location.reload();
+                            } else {
+                                console.error(resp);
+                            }
+                        });   
+                });
+            })
+
+    } else {
+        console.error("media devices not supported!")
+    }
+
+})();
+
